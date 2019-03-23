115bfe756b37c291badb101e81dfcb8ce6ee917b
Dominic Smorra <dominicsmorra@gmail.com>
2019-03-22 21:27:41 -0400

    message

--- app/src/main/java/edu/quinnipiac/myapplication/QuoteHandler.java
+++ app/src/main/java/edu/quinnipiac/myapplication/QuoteHandler.java
@@ -1,6 +1,5 @@
 package edu.quinnipiac.myapplication;
 
-import android.util.Log;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -8,10 +7,6 @@ import org.json.JSONObject;
 
 public class QuoteHandler {
 
-    private static final int START_YEAR = 1950;
-    private static final int END_YEAR= 2010;
-    public static final String YEAR_FACT = "YEAR_FACT";
-    final public static String [] years = new String[END_YEAR - START_YEAR +1];;
 
     public QuoteHandler(){
 
